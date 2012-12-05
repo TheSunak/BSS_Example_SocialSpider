5d12095c711f38ee45bea21f4f914faf5a0699e8
Vishal Sunak <vishalsunak@gmail.com>
2012-12-05 10:46:18 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialSpider::Application.config.secret_token = 'a303ee48a606943006046a40801f4db5269ab885d232bb98b6c5ff8847b9a371e2784c1f5ad04d39cbc91a864dbddf3a322f92b1c7d3e6ea63e0ae770b92cb1f'
